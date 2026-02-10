8cfa8f02f04971227875881a1de640a65d65958e
bangadjie <masadjie927@gmail.com>
2026-02-10 09:20:28 +0700

    Perulangan

--- Jobsheet1/Perulangan.java
+++ Jobsheet1/Perulangan.java
@@ -1,7 +1,33 @@
 package Jobsheet1;
-
+import java.util.*;
 public class Perulangan {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan NIM");
+        double NIM = sc.nextDouble();
+        double n = (NIM % 100);
+
+        if(n < 10){
+            n += 10;
+        }
+        System.out.println("n : " + n);
+        System.out.println("========================");
+
+        for (int i = 1; i <= n; i++) {
+            
+            if (i == 10 || i == 15) {
+                continue; 
+            }if (i % 3 == 0) {
+                System.out.print("# ");
+            }else if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }else {
+                System.out.print("* ");
+            }
+        }
         
+        System.out.println("\n===============================");
+        sc.close(); 
     }
 }
