84de0693facdf0979891b293151768c9b6816920
bangadjie <masadjie927@gmail.com>
2026-02-24 10:16:06 +0700

    Update Modifikasi

--- Jobsheet2/Mahasiswa22.java
+++ Jobsheet2/Mahasiswa22.java
@@ -1,43 +1,48 @@
 package Jobsheet2;
-// import java.util.Scanner;
 
 public class Mahasiswa22 {
-    
-        String nama;
-        String nim;
-        String kelas;
-        double ipk;
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
 
-    public Mahasiswa22(){
+    public Mahasiswa22() {
     }
-    public Mahasiswa22(String nm, String nim, double ipk, String kls){
+
+    public Mahasiswa22(String nm, String nim, double ipk, String kls) {
         nama = nm;
         this.nim = nim;
         this.ipk = ipk;
         kelas = kls;
     }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
         System.out.println("IPL: " + ipk);
         System.out.println("Kelas: " + kelas);
     }
-    void ubahKelas(String kelasBaru){
+
+    void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
 
-    void updateIpk(double ipkBaru){
-        ipk = ipkBaru;
+    void updateIpk(double ipkBaru) {
+        if (ipk >= 0.0 && ipk <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
     }
 
-    String nilaiKinerja(){
+    String nilaiKinerja() {
         if (ipk >= 3.5) {
             return "Kinerja Sangat Baik";
-        }else if(ipk >= 3.0){
+        } else if (ipk >= 3.0) {
             return "Kinerja baik";
-        }else if(ipk >= 2.0){
+        } else if (ipk >= 2.0) {
             return "Kinerja cukup";
-        }else{
+        } else {
             return "Kinerja kurang";
         }
     }
