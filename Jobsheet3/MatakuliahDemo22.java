a5ca50fdb4bf9ffe2740d89882a500a8acd25546
bangadjie <masadjie927@gmail.com>
2026-02-24 09:41:58 +0700

    Praktikum 3

---
+++ Jobsheet3/MatakuliahDemo22.java
@@ -0,0 +1,38 @@
+package Jobsheet3;
+
+import java.util.Scanner;
+
+public class MatakuliahDemo22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        Matakuliah22 [] arrayOfMatakuliah = new Matakuliah22[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("Kode : ");
+            kode = sc.nextLine();
+            System.out.print("Nama : ");
+            nama = sc.nextLine();
+            System.out.print("Sks : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("-----------------------------");
+
+            arrayOfMatakuliah[i] = new Matakuliah22 (kode, nama, sks,jumlahJam);
+        }
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode          : "+ arrayOfMatakuliah[i].kode);
+            System.out.println("Nama          : "+ arrayOfMatakuliah[i].nama);
+            System.out.println("Sks           : "+ arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah Jam    : "+ arrayOfMatakuliah[i].jumlahJam);
+            System.out.println("---------------------------------");
+        }
+    }
+}
