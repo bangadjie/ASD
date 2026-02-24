55c719dbd4af7e0f9ed1ac4b4a5bef952265a24f
bangadjie <masadjie927@gmail.com>
2026-02-24 09:41:43 +0700

    Praktikum 1 & 2

--- Jobsheet3/MahasiswaDemo22.java
+++ Jobsheet3/MahasiswaDemo22.java
@@ -25,10 +25,10 @@ public class MahasiswaDemo22 {
         }
         for(int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM     : "+ arrayOfMahasiswa[i].nim);
-            System.out.println("Nama     : "+ arrayOfMahasiswa[i].nama);
+            System.out.println("NIM       : "+ arrayOfMahasiswa[i].nim);
+            System.out.println("Nama      : "+ arrayOfMahasiswa[i].nama);
             System.out.println("Kelas     : "+ arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : "+ arrayOfMahasiswa[i].ipk);
+            System.out.println("IPK       : "+ arrayOfMahasiswa[i].ipk);
             System.out.println("------------------------------");
         }
     }
