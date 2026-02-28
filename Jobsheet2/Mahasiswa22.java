b6546c5a62d5e38ef2261c2da936d1ba81c64d30
bangadjie <masadjie927@gmail.com>
2026-02-28 12:17:25 +0700

    Update Percobaan

--- Jobsheet2/Mahasiswa22.java
+++ Jobsheet2/Mahasiswa22.java
@@ -8,25 +8,21 @@ public class Mahasiswa22 {
 
     public Mahasiswa22() {
     }
-
     public Mahasiswa22(String nm, String nim, double ipk, String kls) {
         nama = nm;
         this.nim = nim;
         this.ipk = ipk;
         kelas = kls;
     }
-
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
         System.out.println("IPL: " + ipk);
         System.out.println("Kelas: " + kelas);
     }
-
     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
-
     void updateIpk(double ipkBaru) {
         if (ipk >= 0.0 && ipk <= 4.0) {
             ipk = ipkBaru;
@@ -34,7 +30,6 @@ public class Mahasiswa22 {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
     }
-
     String nilaiKinerja() {
         if (ipk >= 3.5) {
             return "Kinerja Sangat Baik";
