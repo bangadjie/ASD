6628bff3cf815be6317a638a3ae4f7aac860b97d
bangadjie <masadjie927@gmail.com>
2026-03-10 09:40:10 +0700

    Modifikasi

--- Jobsheet5/Faktorial.java
+++ Jobsheet5/Faktorial.java
@@ -1,13 +1,15 @@
 package Jobsheet5;
 
 public class Faktorial {
-    int faktorialBF(int n){
-        int fakto = 1;
-        for(int i = 1; i <= n; i++) {
-            fakto = fakto * i;
-        }
-        return fakto;
+    int faktorialBF(int n) {
+    int fakto = 1;
+    int i = 1; 
+    while (i <= n) { 
+        fakto = fakto * i;
+        i++; 
     }
+    return fakto;
+}
     int faktorialDC(int n){
         if (n==1) {
             return 1;
